4a46a01865cced2e904267fcad6facde9ba48352
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-15 17:44:25 +0300

    feat: index.html de ki veriler için ilk modelimizi oluşturduk

--- scv/admin.py
+++ scv/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import GeneralSetting
+
+@admin.register(GeneralSetting)
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'description', 'parameter', 'created_date', 'updated_date')
+    search_fields = ('name', 'description', 'parameter')
+
+    class Meta:
+        model = GeneralSetting
+        fields = '__all__'
--- scv/models.py
+++ scv/models.py
@@ -1,3 +1,24 @@
+from pyexpat import model
+from turtle import update
+from venv import create
 from django.db import models
 
 # Create your models here.
+
+
+class GeneralSetting(models.Model):
+    name = models.CharField(
+        max_length=100, default='SeçGEO', blank=True, verbose_name='Name', help_text="Enter the name of the site")
+    description = models.TextField(
+        max_length=254, default='A short description about the site.', blank=True, verbose_name='Description', help_text="Enter a short description about the site")
+    parameter = models.TextField(
+        max_length=254, blank=True, verbose_name='Parameter', help_text="Enter any additional parameter")
+
+    def __str__(self):
+        # return self.site_name
+        return f"{self.site_name}"
+
+    class Meta:
+        verbose_name = 'General Setting'
+        verbose_name_plural = 'General Settings'
+        ordering = ['-created_date']
