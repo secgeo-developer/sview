dfb233ba3d5c2043fd70d035c67fddbaba7422be
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-15 17:03:30 +0300

    feat: index.html template dosyasının django html etiketleri formatında düzenlenmesi

--- sview/settings.py
+++ sview/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
