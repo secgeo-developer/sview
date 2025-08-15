f317498a3fe2e85812afeb3725a3c920e880c35e
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-15 20:39:29 +0300

    refactor:  general setting modül ile template arasında dinamik ilişki kurmaya devam ediyoruz.

--- scv/views.py
+++ scv/views.py
@@ -1,4 +1,4 @@
-import site
+
 from django.shortcuts import render
 from scv.models import GeneralSetting
 
@@ -17,6 +17,14 @@ def sview_context(request):
     # GeneralSetting
     site_title = get_general_setting('site_title')
     site_keywords = get_general_setting('site_keywords')
+    site_description = get_general_setting('site_description')
+    site_author = get_general_setting('site_author')
+    home_banner_area_jobs = get_general_setting('home_banner_area_jobs')
+    home_banner_area_birth_date = get_general_setting('home_banner_area_birth_date')
+    home_banner_area_phone = get_general_setting('home_banner_area_phone')
+    home_banner_area_email = get_general_setting('home_banner_area_email')
+    home_banner_area_address = get_general_setting('home_banner_area_address')
+    welcome_area_myself = get_general_setting('welcome_area_myself')
 
     # ImageSetting
 
@@ -33,7 +41,14 @@ def sview_context(request):
     return {
         'site_title': site_title,
         'site_keywords': site_keywords,
-
+        'site_description': site_description,
+        'site_author': site_author,
+        'home_banner_area_jobs': home_banner_area_jobs,
+        'home_banner_area_birth_date': home_banner_area_birth_date,
+        'home_banner_area_phone': home_banner_area_phone,
+        'home_banner_area_email': home_banner_area_email,
+        'home_banner_area_address': home_banner_area_address,
+        'welcome_area_myself': welcome_area_myself,
     }
 
 
