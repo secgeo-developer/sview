5343854ba188a5647b90492dcd615fdf480919ce
test_seccgeo <hamdi.secilmis@gmail.com>
2025-08-16 18:59:00 +0300

    feat: Bütün static dosyaların, üretim sunucuların da farkedilmesi için staticfiles dosyasında toplanması sağlandı.

--- sview/settings.py
+++ sview/settings.py
@@ -123,14 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
-STATIC_URL = 'static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
